91a34104e961ef51a04e6b0bcd8572a2b8a98638
Maciej Gąsiorowski <maciej.gasiorowski@grupawp.pl>
2018-12-11 20:36:13 +0100

    review

--- frontend/puppeteer/__tests__/check-request.js
+++ frontend/puppeteer/__tests__/check-request.js
@@ -3,20 +3,16 @@ var fs = require('fs');
 const parse = require('csv-parse/lib/sync')
 
 describe('Tracking script', () => {
-  let data = []
-
   const fileData = fs.readFileSync('__tests__/resources/test_data.csv')
-  const rows = parse(fileData, {columns: false, trim: true})
-  data = rows
+  const data =  parse(fileData, {columns: false, trim: true})
 
   beforeAll(async () => {
     jest.setTimeout(35000);
-    page = await browser.newPage()
   })
 
   console.log(data)
   test.each(data)(
-    'request should be send when I run javascript code',
+    'request should be send when I run javascript code on %s ',
     async (url, traceName, scriptPath, scriptRunCommand, urlToFInd) => {
       // trace all requests (more https://github.com/GoogleChrome/puppeteer/blob/v1.2.0/docs/api.md#tracingstartoptions)
       await page.tracing.start({path: traceName});
@@ -26,9 +22,9 @@ describe('Tracking script', () => {
       // run script (more https://github.com/GoogleChrome/puppeteer/blob/v1.2.0/docs/api.md#pageevaluatepagefunction-args)
       let result = await page.evaluate(scriptRunCommand);
       const buffer = await page.tracing.stop();
-      let json = JSON.parse(buffer);
+      const json = JSON.parse(buffer);
       // check if request sended
-      let sendedRequest = json.traceEvents.some(function(item) {
+      const sendedRequest = json.traceEvents.some(function(item) {
         if(item && item.args && item.args.data && item.args.data.url) {
           return item.args.data.url.includes(urlToFInd)
         }
